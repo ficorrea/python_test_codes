596f10e012bec1b1a88edd72a166c31727b3502e
ficorrea <fisrael.correa@gmail.com>
2023-06-29 21:10:26 -0300

    python packages

---
+++ python_package/Configurações pro setup.py
@@ -0,0 +1,6 @@
+Mais detalhado
+https://godatadriven.com/blog/a-practical-guide-to-using-setup-py/
+
+https://packaging.python.org/pt_BR/latest/guides/distributing-packages-using-setuptools/#setup-py
+
+https://dev.to/fabianoflorentino/setuptools-gerenciando-pacotes-python-com-facildiade-3a0m
